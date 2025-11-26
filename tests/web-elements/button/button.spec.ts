e1036381c0f0cab524925365e16805eaee709f0e
Nam Automation <namvh@opentechiz.com>
2025-11-26 22:04:07 +0700

    [ID-03-V2] Format code

--- tests/web-elements/button/button.spec.ts
+++ tests/web-elements/button/button.spec.ts
@@ -1,21 +1,36 @@
-import { test, expect, Page } from '@playwright/test';
-let listButton = ['Div button','Origin button','Input button','Default','Primarys','Dashed','Text','Link','Icon button'];
+import { test, expect, Page } from "@playwright/test";
+let listButton = [
+  "Div button",
+  "Origin button",
+  "Input button",
+  "Default",
+  "Primarys",
+  "Dashed",
+  "Text",
+  "Link",
+  "Icon button",
+];
 
-for(let buttonText  of listButton){
-    test(`Verify button ${buttonText}`, async({page}) => {
-    await page.goto('https://test-with-me-app.vercel.app/learning/web-elements/elements/button')
-        // Click on button 
-        await clickButtonByLabel(buttonText,page);
-        // Verify text 
-        await expect(page.getByText(`Button ${buttonText} was clicked!`)).toBeVisible();
-    
-})
+for (let buttonText of listButton) {
+  test(`Verify button ${buttonText}`, async ({ page }) => {
+    await page.goto(
+      "https://test-with-me-app.vercel.app/learning/web-elements/elements/button"
+    );
+    // Click on button
+    await clickButtonByLabel(buttonText, page);
+    // Verify text
+    await expect(
+      page.getByText(`Button ${buttonText} was clicked!`)
+    ).toBeVisible();
+  });
 }
 
-function clickButtonByLabel(label : string, page : Page){
-    //div[normalize-space(.)="Div button"] | //button[normalize-space(.)="Div button"] | //input[normalize-space(.)="Div button"]
-    let divButtonXpath = `//div[normalize-space(.)="${label}"]`
-    let inputButtonXpath = `//input[normalize-space(@value)="${label}"]`
-    let buttonXpath = `//button[normalize-space(.)="${label}"]`
-    page.locator(`${divButtonXpath} | ${inputButtonXpath} | ${buttonXpath}`).click();
-} 
+function clickButtonByLabel(label: string, page: Page) {
+  //div[normalize-space(.)="Div button"] | //button[normalize-space(.)="Div button"] | //input[normalize-space(.)="Div button"]
+  let divButtonXpath = `//div[normalize-space(.)="${label}"]`;
+  let inputButtonXpath = `//input[normalize-space(@value)="${label}"]`;
+  let buttonXpath = `//button[normalize-space(.)="${label}"]`;
+  page
+    .locator(`${divButtonXpath} | ${inputButtonXpath} | ${buttonXpath}`)
+    .click();
+}
