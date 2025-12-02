62ed99eaad4c6c682453c0a07962b6fa4397a965
Nam Automation <namvh@opentechiz.com>
2025-12-02 23:23:54 +0700

    [ID-07] Verify radio

---
+++ tests/web-elements/radio/radio.spec.ts
@@ -0,0 +1,28 @@
+import{test,Page, expect} from "@playwright/test"
+let textRadios =["Apple","Pear","Orange"]
+let xpathDivRadios = "//div[contains(concat(' ',normalize-space(@class),' '),' ant-radio-group ')]"
+test("Verify radio", async({page})=>{
+    await page.goto("https://test-with-me-app.vercel.app/learning/web-elements/elements/radio");
+    const qtyDivRadio = await getQtyElementRadio(xpathDivRadios,page);
+    console.log("qty div radio :", qtyDivRadio);
+    if(qtyDivRadio > 0){
+        for (let index = 1; index <= qtyDivRadio; index++) {
+        let xpathDivRadio = `//div[contains(concat(' ',normalize-space(@class),' '),' ant-radio-group ')][${index}]`
+        let xpathValue = `//div[normalize-space(text())='Value:'][${index}]//span`;
+    for(let textRadio of textRadios){
+        //label[.//span[normalize-space(text())="Apple"]]
+        let xpathRadio = xpathDivRadio +`//label[.//span[normalize-space(text())="${textRadio}"]]`
+        await page.locator(xpathRadio).click();
+        await expect(page.locator(xpathValue).getByText(`${textRadio}`)).toBeVisible();
+    }}}
+})
+
+async function getQtyElementRadio(xpath:string, page:Page): Promise<number> {
+    try {
+        const count = page.locator(xpath).count();
+        return count;
+    } catch (error) {
+        console.log(`Loi khi dem so luong element : ${xpath}`, error);
+        return 0;
+    }
+}
