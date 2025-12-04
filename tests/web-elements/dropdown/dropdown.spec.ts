7c240a42c8bf4995826ceb0d7f714d0d95f22abf
vo nam <namvh@leadsgen.asia>
2025-12-04 08:41:37 +0700

    [ID-08] Verify Dropdown

---
+++ tests/web-elements/dropdown/dropdown.spec.ts
@@ -0,0 +1,14 @@
+import{test,Page, expect} from "@playwright/test"
+let textDropdowns =["1st menu item","2nd menu item","3rd menu item","4rd menu item"]
+for(let textDropdown of textDropdowns){
+test(`Verify Dropdown ${textDropdown}`, async({page})=>{
+    await page.goto("https://test-with-me-app.vercel.app/learning/web-elements/elements/dropdown")
+    let xpathDropdown = "//button[contains(concat(' ',normalize-space(@class),' '),' ant-dropdown-trigger ')]";
+    await page.locator(xpathDropdown).hover();
+    let xpathItemDropdown = `//li[.//span[normalize-space(text())='${textDropdown}']]`
+    await page.locator(xpathItemDropdown).click();
+    let xpathValue = "//div[contains(text(), 'Value:')]//span";
+    let xpathValueButton = `//button[.//span[normalize-space(text())="${textDropdown}"]]`
+    expect(page.locator(xpathValue).getByText(`${textDropdown}`)).toBeVisible();
+    expect(page.locator(xpathValueButton)).toBeVisible();
+})}
