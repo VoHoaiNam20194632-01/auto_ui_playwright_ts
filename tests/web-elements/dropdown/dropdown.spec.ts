23b348080c0e8196eeb742982d96e0d2122ea4d5
Nam Automation <namvh@opentechiz.com>
2025-12-08 22:23:39 +0700

    [ID-08-V2] Verify Dropdown and Refartor

--- tests/web-elements/dropdown/dropdown.spec.ts
+++ tests/web-elements/dropdown/dropdown.spec.ts
@@ -3,11 +3,16 @@ let textDropdowns =["1st menu item","2nd menu item","3rd menu item","4rd menu it
 for(let textDropdown of textDropdowns){
 test(`Verify Dropdown ${textDropdown}`, async({page})=>{
     await page.goto("https://test-with-me-app.vercel.app/learning/web-elements/elements/dropdown")
-    let xpathDropdown = "//button[contains(concat(' ',normalize-space(@class),' '),' ant-dropdown-trigger ')]";
-    await page.locator(xpathDropdown).hover();
-    let xpathItemDropdown = `//li[.//span[normalize-space(text())='${textDropdown}']]`
-    await page.locator(xpathItemDropdown).click();
+    await selectDropdownByLabel('Dropdown',textDropdown,page);
     let xpathValueButton = `//button[.//span[normalize-space(text())="${textDropdown}"]]`
     expect(page.getByText(`Value: ${textDropdown}`)).toBeVisible();
     expect(page.locator(xpathValueButton)).toBeVisible();
 })}
+
+async function selectDropdownByLabel(label:string,option:string,page:Page){
+    //div[normalize-space()="Dropdown"]/following-sibling::div//button[contains(concat(' ',normalize-space(@class),' '),' ant-dropdown-trigger ')]
+    let openDropdownXpath=`//div[normalize-space()="${label}"]/following::button[contains(concat(' ',normalize-space(@class),' '),' ant-dropdown-trigger ')][1]`;
+    await page.locator(openDropdownXpath).hover();
+    let dropdownOptionXpath =`//li[normalize-space(.)='${option}']`;
+    await page.locator(dropdownOptionXpath).click();
+}
