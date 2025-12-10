29af2843b2b65d66b9b722188133ac09800a943f
vo nam <namvh@leadsgen.asia>
2025-12-10 20:49:17 +0700

    [ID-10-V2] Verify Half Rate

--- tests/rating/rating.spec.ts
+++ tests/rating/rating.spec.ts
@@ -22,3 +22,36 @@ async function selectRatingByLabel(label:string, rating: number,page:Page){
     await ratingGroupLocator.locator(xpath).click();
     }
 }
+
+test(`Verify select half rating  `, async ({ page }) => {
+    await page.goto(
+        "https://test-with-me-app.vercel.app/learning/web-elements/components/rating"
+    );
+    await selectHalfRatingByLabel('Haft Rate', 2.5, page);
+    // Verify text
+    await expect(
+        page.getByText(`Current rating: 2.5`)
+    ).toBeVisible();
+});
+
+async function selectHalfRatingByLabel(label: string, rating:number, page:Page) {
+    let ratingGroupXpath = `(//div[normalize-space()="${label}"]/following::ul[@role="radiogroup"])[1]`
+    let ratingGroupLocator = page.locator(ratingGroupXpath);
+    let currentFullRating = (await ratingGroupLocator.locator(`//li[contains(concat(' ',normalize-space(@class),' '),' ant-rate-star-full ')]`).all()).length;
+    let currentHalfRating = (await ratingGroupLocator.locator(`//li[contains(concat(' ',normalize-space(@class),' '),' ant-rate-star-half ')]`).all()).length;
+    if(currentHalfRating > 0){
+        currentFullRating = currentFullRating + 0.5;
+    }
+    if(rating != currentFullRating){
+        let xpath;
+    if(Number.isInteger(rating)){
+        xpath =  `//li[${rating}]//div[contains(concat(' ',normalize-space(@class),' '),' ant-rate-star-second ')]`
+    }else{
+        xpath =  `//li[${rating + 0.5}]//div[contains(concat(' ',normalize-space(@class),' '),' ant-rate-star-first ')]`
+    }
+    await ratingGroupLocator.locator(xpath).click();
+}
+}
+
+
+
