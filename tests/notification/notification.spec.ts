c0be76021af516e6ac49d8c5247516b8b708ab4b
Nam Automation <namvh@opentechiz.com>
2025-12-25 22:36:08 +0700

    [ID-19-V3] Refator Notification

--- tests/notification/notification.spec.ts
+++ tests/notification/notification.spec.ts
@@ -7,24 +7,24 @@ test(`Verify Notification ${textNotification}`, async ({ page }) => {
     "https://test-with-me-app.vercel.app/learning/web-elements/components/notification"
   );
   // Click on button
-  await selectButtonOnNotificationAndVerify(textNotification,page);
+  await verifyNotificationMessage(textNotification,page);
 });
 }
 
-async function selectButtonOnNotificationAndVerify(modalName:string, page:Page) {
+async function verifyNotificationMessage(messageType:string, page:Page) {
     //click button button Notification
-    let buttonModalXpath = `//div[.//span[normalize-space(text())="Notification"] and @role="separator"]/following::div[contains(concat(' ',normalize-space(@class),' '),' ant-space-item ')]//button[normalize-space(.)="${modalName}"]`
-    await page.locator(buttonModalXpath).click();
-    await expect(page.getByText(`Notification ${modalName}`)).toBeVisible();
-    await expect(page.getByText(`You have clicked the ${modalName} button.`)).toBeVisible();
-    let iconNotificationXpath = `//div[contains(concat(' ',normalize-space(@class),' '),' ant-notification-notice-with-icon ')]//span[@role="img"]`
-    let iconNotificationLocator = page.locator(iconNotificationXpath);
-    const expectedClassPart = `ant-notification-notice-icon-${modalName.toLowerCase()}`;
-    await expect(iconNotificationLocator, `Lỗi sai Notification icon "${modalName}, class actual : ${iconNotificationLocator.getAttribute('class')}"`).toHaveClass(
-        new RegExp(expectedClassPart),  // Dùng regex để check chứa phần class mong muốn
-        { timeout: 5000 }
-    );
-    // let modalNameUpper = modalName.toUpperCase();
-    // let buttonCloseNotification = `//div[contains(concat(' ',normalize-space(@class),' '),' ant-notification-notice-wrapper ') and .//div[contains(concat(' ',normalize-space(text()),' '),' Notification ${modalNameUpper} ')]]//a[@aria-label="Close"]`;
-    // await page.locator(buttonCloseNotification).click();
+    let buttonXpath = `//div[.//span[normalize-space(text())="Notification"] and @role="separator"]/following::div[contains(concat(' ',normalize-space(@class),' '),' ant-space-item ')]//button[normalize-space(.)="${messageType}"]`
+    await page.locator(buttonXpath).click();
+    await expect(page.getByText(`Notification ${messageType}`)).toBeVisible();
+    await expect(page.getByText(`You have clicked the ${messageType} button.`)).toBeVisible();
+    // let iconNotificationXpath = `//div[contains(concat(' ',normalize-space(@class),' '),' ant-notification-notice-with-icon ')]//span[@role="img"]`
+    // let iconNotificationLocator = page.locator(iconNotificationXpath);
+    // const expectedClassPart = `ant-notification-notice-icon-${messageType.toLowerCase()}`;
+    // await expect(iconNotificationLocator, `Lỗi sai Notification icon "${messageType}, class actual : ${iconNotificationLocator.getAttribute('class')}"`).toHaveClass(
+    //     new RegExp(expectedClassPart),  // Dùng regex để check chứa phần class mong muốn
+    //     { timeout: 5000 }
+    // );
+    let modalNameUpper = messageType.toUpperCase();
+    let buttonCloseNotification = `//div[contains(concat(' ',normalize-space(@class),' '),' ant-notification-notice-wrapper ') and .//div[contains(concat(' ',normalize-space(text()),' '),' Notification ${modalNameUpper} ')]]//a[@aria-label="Close"]`;
+    await page.locator(buttonCloseNotification).click();
 }
