f949a425550e983004a82fd98340016f0a659e69
Nam Automation <namvh@opentechiz.com>
2026-01-12 20:37:41 +0700

    [ID-26] Verify Iframe

---
+++ tests/iframe/iframe.spec.ts
@@ -0,0 +1,10 @@
+import{test,Page, expect} from "@playwright/test"
+
+
+test(`Verify Iframe `, async({page})=>{
+    await page.goto("https://test-with-me-app.vercel.app/learning/web-elements/frames")
+    let frameXpath = `//iframe[@title="Example Frame"]`;
+    const iframe = page.locator(frameXpath).contentFrame();
+    await iframe.getByText('API Automation Java/JS').click();
+    await expect(iframe.getByText('Nâng tầm kỹ năng kiểm thử, chinh phục đỉnh cao sự nghiệp với khóa học API Automation Testing chuyên sâu.')).toBeVisible();
+});
