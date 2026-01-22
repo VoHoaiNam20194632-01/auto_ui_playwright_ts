45dc570103416e72c49c64382d7b74261f25443c
Nam Automation <namvh@opentechiz.com>
2026-01-22 22:30:00 +0700

    [ID-34] Verify server errors

--- tests/evershop/new-product/new-product.spec.ts
+++ tests/evershop/new-product/new-product.spec.ts
@@ -63,3 +63,76 @@ test("Verify create new product", async ({ page, request }) => {
     let productId = editProductPage.getProductId();
     productIds.push(productId)
 });
+
+test("Verify create new product 2", async ({ page, request }) => {
+    const random = new Date().getTime();
+    console.log('Create new product');
+    await dashboardPage.clickMenuByLabel('New Product');
+    await newProductPage.isDisplay();
+    const productName = `Giày Chạy Bộ Biti's ${random}`;
+    await newProductPage.inputTextByLabel('Product Name', productName);
+    await newProductPage.inputTextByLabel('SKU', `SKU-${random}`);
+    await newProductPage.inputTextByLabel('Price', "50");
+    await newProductPage.inputTextByLabel('Weight', "1");
+    await newProductPage.inputTextByLabel('Quantity', "100");
+    await newProductPage.inputTextByLabel('URL Key', `nam${random}`);
+    await newProductPage.inputTextByLabel('Meta Title', "BITIS, CHAY BO");
+    await newProductPage.selectDropdownByLabel('Tax Class', 'Taxable Goods');
+    await newProductPage.selectDropdownByLabel('Attribute group', 'Default');
+    await newProductPage.clickRadioButtonByLabel('Status', 'Disabled');
+    await newProductPage.clickRadioButtonByLabel('Visibility', 'Not visible individually');
+    await newProductPage.clickRadioButtonByLabel('Manage Stock', 'No');
+    await newProductPage.clickRadioButtonByLabel('Stock Availability', 'Out of Stock');
+    await newProductPage.selectDropdownByLabel('Color', 'Black');
+    await newProductPage.selectDropdownByLabel('Size', 'XXL');
+    await newProductPage.uploadImage('data/new-product/image/DSC_4950.jpg');
+    await newProductPage.inputTextAreaByLabel('Meta Description', 'Meta Description');
+    await newProductPage.clickButtonByLabel('Save');
+    await newProductPage.verifyPopupMessage('Product created successfully');
+    await newProductPage.clickMenuByLabel('Products');
+    productsPage.isDisplay();
+    await productsPage.searchProduct(random.toString());
+    await productsPage.selectProductByName(productName);
+    await editProductPage.isDisplay(`Editing ${productName}`);
+    await expect(await editProductPage.getFieldValueByLabel('Product Name')).toEqual(productName);
+    let productId = editProductPage.getProductId();
+    productIds.push(productId)
+});
+
+test("Verify error message when server return 500 error", async ({ page, request }) => {
+    const random = new Date().getTime();
+    console.log('Create new product');
+    await dashboardPage.clickMenuByLabel('New Product');
+    await newProductPage.isDisplay();
+    const productName = `Giày Chạy Bộ Biti's ${random}`;
+    await newProductPage.inputTextByLabel('Product Name', productName);
+    await newProductPage.inputTextByLabel('SKU', `SKU-${random}`);
+    await newProductPage.inputTextByLabel('Price', "50");
+    await newProductPage.inputTextByLabel('Weight', "1");
+    await newProductPage.inputTextByLabel('Quantity', "100");
+    await newProductPage.inputTextByLabel('URL Key', `nam${random}`);
+    await newProductPage.inputTextByLabel('Meta Title', "BITIS, CHAY BO");
+    await newProductPage.selectDropdownByLabel('Tax Class', 'Taxable Goods');
+    await newProductPage.selectDropdownByLabel('Attribute group', 'Default');
+    await newProductPage.clickRadioButtonByLabel('Status', 'Disabled');
+    await newProductPage.clickRadioButtonByLabel('Visibility', 'Not visible individually');
+    await newProductPage.clickRadioButtonByLabel('Manage Stock', 'No');
+    await newProductPage.clickRadioButtonByLabel('Stock Availability', 'Out of Stock');
+    await newProductPage.selectDropdownByLabel('Color', 'Black');
+    await newProductPage.selectDropdownByLabel('Size', 'XXL');
+    await newProductPage.uploadImage('data/new-product/image/DSC_4950.jpg');
+    await newProductPage.inputTextAreaByLabel('Meta Description', 'Meta Description');
+    page.route('*/**/api/products', async route => {
+        await route.fulfill({
+            json: {
+                error: {
+                    status: 500,
+                    message: "Internal Server Error"
+                }
+            }, status: 500
+        });
+    });
+    await newProductPage.clickButtonByLabel('Save');
+    await newProductPage.verifyPopupMessage('Internal Server Error');
+
+});
