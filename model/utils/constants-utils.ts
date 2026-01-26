f7485b9bd57e3c6089e6e781e68b5a8b173b8072
Nam Automation <namvh@opentechiz.com>
2026-01-26 21:33:36 +0700

    [ID-36] Manage multiple enviroment

--- model/utils/constants-utils.ts
+++ model/utils/constants-utils.ts
@@ -1,6 +1,7 @@
-export const UI_HOST ='http://localhost';
-export const UI_PORT = '3000';
+import { getEnv } from "./config-utils";
+export const UI_HOST = getEnv('UI_HOST');
+export const UI_PORT = getEnv('UI_PORT');
 export const UI_BASE_URL = `${UI_HOST}${UI_PORT ? ':' + UI_PORT : ''}`;
 export const UI_ADMIN_LOGIN_URL = `${UI_BASE_URL}/admin/login`;
-export const ADMIN_USERNAME = `test@with.me`;
-export const ADMIN_PASSWORD = `24081201Nam@`;
+export const ADMIN_USERNAME = getEnv('ADMIN_USERNAME');
+export const ADMIN_PASSWORD = getEnv('ADMIN_PASSWORD');
